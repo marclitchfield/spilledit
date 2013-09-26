ea3318474827afc4dd8bdaf24e602716b71fb5b7
marclitchfield <marclitchfield@gmail.com>
2013-09-25 21:11:04 -0700

    Converted to node.js application

---
+++ app.js
@@ -0,0 +1,19 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
