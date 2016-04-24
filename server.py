5a3ec7a322e54a35d0116e3574e6c48dcd378adb
Marc Litchfield <marclitchfield@gmail.com>
2016-04-24 11:19:04 -0700

    Serve all static resources through flask

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/img/<path:path>')
-def send_img(path):
-    return send_from_directory('static/img', path)
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 @app.route("/")
 def index():
